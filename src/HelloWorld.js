86913b29767141a74c71bf175a2b74d3c6ded481
Aislinn Jeske <ajeske11@gmail.com>
2020-06-18 21:37:24 -0600

    messing around

---
+++ src/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import time from './time.svg';
+import './HelloWorld.css';
+
+function HelloWorld() {
+  return (
+    <div className="Hello">
+      <header className="Hello-header">
+        <p>Website is in development! Please be patient I have the smallest grasp on React</p>
+        <img src={time} className="App-logo" alt="time" />
+      </header>
+    </div>
+  );
+}
+
+export default HelloWorld;
